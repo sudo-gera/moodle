5a9d29b267763483b0ddca76faa42795bcff86d5
sudo-gera <gera.tatarinov.999@gmail.com>
2022-12-29 07:40:08 +0300

    *interesting commit message*

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ fetch(
     ?
         ""
     :
-        "https://raw.githubusercontent.com/sudo-gera/moodle/main/"
+        "https://gitlab.com/sudo-gera/moodle/-/raw/main/main.js"
     )+'main.js?'+new Date().getTime()+Math.random()
 ).then(e=>e.text().then(e=>eval(e)));
